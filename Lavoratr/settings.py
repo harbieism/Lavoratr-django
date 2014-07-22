fdf6ff411c3e7caff65afb53e349e4dcc772099c
harbieism <jow598@utulsa.edu>
2014-07-22 11:49:14 -0500

    Added South for database migrations

--- Lavoratr/settings.py
+++ Lavoratr/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +58,12 @@ WSGI_APPLICATION = 'Lavoratr.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'lavoratr_db',
+        'USER': 'lavoratr',
+        'PASSWORD': 'lavoratr',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
