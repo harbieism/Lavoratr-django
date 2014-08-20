60b88486caa4a6a21bca6ba8cdb2b0beaa8f49d8
harbieism <jow598@utulsa.edu>
2014-08-20 13:55:34 -0500

    Changed anchor of toilet popup.

--- lavoratr/static/lavoratr/toilet_map.js
+++ lavoratr/static/lavoratr/toilet_map.js
@@ -18,7 +18,7 @@ var toiletIcon = L.icon({
     iconUrl: '/static/lavoratr/img/Toilet.png',
     iconSize:     [40, 40], // size of the icon
     iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+    popupAnchor:  [0, -20] // point from which the popup should open relative to the iconAnchor
 });
 
 var map = L.map('map', mapOptions);
