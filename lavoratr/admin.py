9918ae594df42608199124ccb461808871af2c39
harbieism <jow598@utulsa.edu>
2014-09-03 11:03:05 -0500

    Modified admin.py to work with new rating model.

--- lavoratr/admin.py
+++ lavoratr/admin.py
@@ -4,11 +4,14 @@ from lavoratr.models import Toilet
 
 class ToiletAdmin(admin.ModelAdmin):
     fields = [
-        'building', 'location', 'rating',
+        'building', 'location', 'positive_ratings', 'negative_ratings',
         'gender', 'single_occupancy', 'accesible',
         'station', 'created', 'comment_box',
         'lon', 'lat', 'point'
     ]
 
-    list_display = ('building', 'location', 'rating', 'created')
+    list_display = (
+    	'building', 'location', 'positive_ratings', 'negative_ratings',
+    	'created'
+    )
 admin.site.register(Toilet, ToiletAdmin)
