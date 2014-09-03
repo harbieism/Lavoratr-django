dc613c297955479ac1180d91ae8d954b04e0c5c3
harbieism <jow598@utulsa.edu>
2014-09-03 11:20:02 -0500

    Removed 'neutral' option from ratings.

--- lavoratr/forms.py
+++ lavoratr/forms.py
@@ -17,7 +17,7 @@ def validate_length(value):
         raise ValidationError('%s is not longer than 5 characters' % value)
 
 
-'''NOTE:  for rating: 1=like, 2 = dislike, 3 = neutral'''
+'''NOTE:  for rating: 1=like, 2 = dislike'''
 
 
 class ToiletForm(forms.Form):
@@ -29,7 +29,7 @@ class ToiletForm(forms.Form):
         max_length=50, validators=[validate_length], required=True
     )
     gender = forms.ChoiceField(choices=SEX_CHOICES)
-    rating = forms.IntegerField(min_value=1, max_value=3)
+    rating = forms.IntegerField(min_value=1, max_value=2)
     single_occupancy = forms.BooleanField(required=False)
     accesible = forms.BooleanField(required=False)
     station = forms.BooleanField(required=False)
@@ -37,5 +37,5 @@ class ToiletForm(forms.Form):
 
 
 class ReviewForm(forms.Form):
-    rating = forms.IntegerField(min_value=1, max_value=3)
+    rating = forms.IntegerField(min_value=1, max_value=2)
     comment_box = forms.CharField(max_length=127, required=False)
