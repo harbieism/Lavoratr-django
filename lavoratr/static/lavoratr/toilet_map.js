4e83295919916ccfb67df31836837a2842e68e8c
harbieism <jow598@utulsa.edu>
2014-09-05 13:17:17 -0500

    Improved Javascript formatting

--- lavoratr/static/lavoratr/toilet_map.js
+++ lavoratr/static/lavoratr/toilet_map.js
@@ -11,30 +11,30 @@ var mapOptions = {
 
 var youIcon = L.icon({
     iconUrl: '/static/lavoratr/img/peeman.png',
-    iconSize:     [20, 40], // size of the icon
-    iconAnchor:   [10, 20], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, 0] // point from which the popup should open relative to the iconAnchor
+    iconSize: [20, 40], // size of the icon
+    iconAnchor: [10, 20], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, 0] // point from which the popup should open relative to the iconAnchor
 });
 
 var toiletIcon = L.icon({
     iconUrl: '/static/lavoratr/img/Toilet.png',
-    iconSize:     [40, 40], // size of the icon
-    iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -20] // point from which the popup should open relative to the iconAnchor
+    iconSize: [40, 40], // size of the icon
+    iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -20] // point from which the popup should open relative to the iconAnchor
 });
 
 var fToiletIcon = L.icon({
     iconUrl: '/static/lavoratr/img/Toilet_F.png',
-    iconSize:     [40, 40], // size of the icon
-    iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -20] // point from which the popup should open relative to the iconAnchor
+    iconSize: [40, 40], // size of the icon
+    iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -20] // point from which the popup should open relative to the iconAnchor
 });
 
 var mToiletIcon = L.icon({
     iconUrl: '/static/lavoratr/img/Toilet_M.png',
-    iconSize:     [40, 40], // size of the icon
-    iconAnchor:   [20, 20], // point of the icon which will correspond to marker's location
-    popupAnchor:  [0, -20] // point from which the popup should open relative to the iconAnchor
+    iconSize: [40, 40], // size of the icon
+    iconAnchor: [20, 20], // point of the icon which will correspond to marker's location
+    popupAnchor: [0, -20] // point from which the popup should open relative to the iconAnchor
 });
 
 var map = L.map('map', mapOptions);
@@ -52,101 +52,119 @@ L.tileLayer(
 
 
 var restroomLayer = L.geoJson($data, {
-    onEachFeature: onEachFeature,
-    pointToLayer: function (feature, latlng) {
-        if (String(feature.properties.gender) == "M"){
-            layerIcon = mToiletIcon;
-        } else if (String(feature.properties.gender) == "F"){
-            layerIcon = fToiletIcon;
-        } else {
-            layerIcon = toiletIcon;
-        };
-        return L.marker(latlng, {icon: layerIcon, riseOnHover: true});
-    }
-}).addTo(map);
-
-$("#filter").click( function (){
-
-    var genderList = [];
-    var accesible = false;
-    var singleOccupancy = false;
-    var changingStation = false;
-
-    if ($('#female').is(':checked')){
-        genderList.push("F");
-    };
-    if ($('#male').is(':checked')){
-        genderList.push("M");
-    };
-    if ($('#unisex').is(':checked')){
-        genderList.push("U");
-    };
-
-    if ($('#single_occupancy').is(':checked')){
-        singleOccupancy = true;
-    };
-    if ($('#accesible').is(':checked')){
-        accesible = true;
-    };
-    if ($('#station').is(':checked')){
-        changingStation = true;
-    };
-
-    restroomLayer.clearLayers();
-    restroomLayer = L.geoJson($data, {
-        filter: function(feature, layer) {
-
-            var genderTrue = true;
-            var occupancyCheck = true;
-            var accesibleCheck = true;
-            var changingCheck = true;
-
-            if (genderList.indexOf(String(feature.properties.gender)) < 0){
-                genderTrue = false;
-            }
-            console.log("GenderTrue" + genderTrue);
-            if (singleOccupancy == true){
-                if (feature.properties.single_occupancy == false){
-                    occupancyCheck = false;
-                };
-            };
-            console.log("Occupancy" + occupancyCheck);
-            if (accesible == true){
-                if (feature.properties.accesible == false){
-                    accesibleCheck = false;
-                };
-            };
-            console.log("Accesible" + accesibleCheck);
-            if (changingStation == true){
-                if (feature.properties.station == false){
-                    changingCheck = false;
-                }
-            }
-            console.log('Changing' + changingCheck);
-            if (genderTrue && occupancyCheck && accesibleCheck && changingCheck){
-                return true;
-            } else {
-                return false;
-            }
-
-        },
         onEachFeature: onEachFeature,
-        pointToLayer: function (feature, latlng) {
-        if (String(feature.properties.gender) == "M"){
-            layerIcon = mToiletIcon;
-        } else if (String(feature.properties.gender) == "F"){
-            layerIcon = fToiletIcon;
-        } else {
-            layerIcon = toiletIcon;
-        };
-            return L.marker(latlng, {icon: layerIcon, riseOnHover: true});
+        pointToLayer: function(feature, latlng) {
+            if (String(feature.properties.gender) == "M") {
+                layerIcon = mToiletIcon;
+            } else if (String(feature.properties.gender) == "F") {
+                layerIcon = fToiletIcon;
+            } else {
+                layerIcon = toiletIcon;
+            };
+            return L.marker(latlng, {
+                icon: layerIcon,
+                riseOnHover: true
+            });
         }
-    }).addTo(map);
-});
+    })
+    .addTo(map);
+
+$("#filter")
+    .click(function() {
+
+        var genderList = [];
+        var accesible = false;
+        var singleOccupancy = false;
+        var changingStation = false;
+
+        if ($('#female')
+            .is(':checked')) {
+            genderList.push("F");
+        };
+        if ($('#male')
+            .is(':checked')) {
+            genderList.push("M");
+        };
+        if ($('#unisex')
+            .is(':checked')) {
+            genderList.push("U");
+        };
+
+        if ($('#single_occupancy')
+            .is(':checked')) {
+            singleOccupancy = true;
+        };
+        if ($('#accesible')
+            .is(':checked')) {
+            accesible = true;
+        };
+        if ($('#station')
+            .is(':checked')) {
+            changingStation = true;
+        };
+
+        restroomLayer.clearLayers();
+        restroomLayer = L.geoJson($data, {
+                filter: function(feature, layer) {
+
+                    var genderTrue = true;
+                    var occupancyCheck = true;
+                    var accesibleCheck = true;
+                    var changingCheck = true;
+
+                    if (genderList.indexOf(String(feature.properties.gender)) < 0) {
+                        genderTrue = false;
+                    }
+                    console.log("GenderTrue" + genderTrue);
+                    if (singleOccupancy == true) {
+                        if (feature.properties.single_occupancy == false) {
+                            occupancyCheck = false;
+                        };
+                    };
+                    console.log("Occupancy" + occupancyCheck);
+                    if (accesible == true) {
+                        if (feature.properties.accesible == false) {
+                            accesibleCheck = false;
+                        };
+                    };
+                    console.log("Accesible" + accesibleCheck);
+                    if (changingStation == true) {
+                        if (feature.properties.station == false) {
+                            changingCheck = false;
+                        }
+                    }
+                    console.log('Changing' + changingCheck);
+                    if (genderTrue && occupancyCheck && accesibleCheck && changingCheck) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+
+                },
+                onEachFeature: onEachFeature,
+                pointToLayer: function(feature, latlng) {
+                    if (String(feature.properties.gender) == "M") {
+                        layerIcon = mToiletIcon;
+                    } else if (String(feature.properties.gender) == "F") {
+                        layerIcon = fToiletIcon;
+                    } else {
+                        layerIcon = toiletIcon;
+                    };
+                    return L.marker(latlng, {
+                        icon: layerIcon,
+                        riseOnHover: true
+                    });
+                }
+            })
+            .addTo(map);
+    });
 
 
 map.on('locationfound', onLocationFound);
-map.locate({setView: true, maxZoom: 17});
+map.locate({
+    setView: true,
+    maxZoom: 17
+});
 
 
 function onEachFeature(feature, layer) {
@@ -159,15 +177,15 @@ function onEachFeature(feature, layer) {
         popupString += 'Rating: ' + realRating + '<br />';
         popupString += 'Gender: ' + feature.properties.gender + '<br />';
         link = (
-            "<div class='detail_link'><a href=/detail/"
-            + feature.properties.id + "/>detail</a></div>"
+            "<div class='detail_link'><a href=/detail/" + feature.properties.id +
+            "/>detail</a></div>"
         );
         popupString += link + '</div>';
         layer.bindPopup(popupString);
     };
 
     var hoverStyle = {
-        color: '#2262CC', 
+        color: '#2262CC',
         weight: 3,
         opacity: 0.6,
         fillOpacity: 0.65,
@@ -183,56 +201,69 @@ function onEachFeature(feature, layer) {
     };
 
     if (!(layer instanceof L.Point)) {
-        layer.on('mouseover', function () {
-        });
-        layer.on('mouseout', function () {
-        });
+        layer.on('mouseover', function() {});
+        layer.on('mouseout', function() {});
     }
 };
-    
+
 
 function onLocationFound(e) {
     var radius = e.accuracy / 2;
     var lat = e.latlng.lat;
     var lng = e.latlng.lng;
-    $('#lat').val(parseFloat(lat.toString()));
-    $('#lng').val(parseFloat(lng.toString()));
+    $('#lat')
+        .val(parseFloat(lat.toString()));
+    $('#lng')
+        .val(parseFloat(lng.toString()));
 
 
 
-    var userMarker = L.marker(e.latlng, {icon: youIcon, draggable: true}).addTo(map);
-    
-    L.circle(e.latlng, radius).addTo(map);
+    var userMarker = L.marker(e.latlng, {
+            icon: youIcon,
+            draggable: true
+        })
+        .addTo(map);
 
-    userMarker.on('dragend', function(e){
+    L.circle(e.latlng, radius)
+        .addTo(map);
+
+    userMarker.on('dragend', function(e) {
         var coords = e.target.getLatLng();
         lat = coords.lat;
         lng = coords.lng;
-        $('#lat').val(parseFloat(lat.toString()));
-        $('#lng').val(parseFloat(lng.toString()));
+        $('#lat')
+            .val(parseFloat(lat.toString()));
+        $('#lng')
+            .val(parseFloat(lng.toString()));
     });
 
-    $('#add_restroom').click(function (event) {
-        window.location="add_toilet/" + lat + "/" + lng + "/";
-    });
+    $('#add_restroom')
+        .click(function(event) {
+            window.location = "add_toilet/" + lat + "/" + lng + "/";
+        });
 }
 
-$('#slider_open_button').click( function () {
-    if (slider_out == true){
-        $('#slider_open_button').animate({
-            right: "+=250"
-        });
-        $('#right_slider').animate({
-            right: "+=250"
-        });
-        slider_out = false;
-    } else {
-        $('#slider_open_button').animate({
-            right: "-=250"
-        });
-        $('#right_slider').animate({
-            right: "-=250"
-        });
-        slider_out = true;
-    }
-});
+$('#slider_open_button')
+    .click(function() {
+        if (slider_out == true) {
+            $('#slider_open_button')
+                .animate({
+                    right: "+=250"
+                });
+            $('#right_slider')
+                .animate({
+                    right: "+=250"
+                });
+            slider_out = false;
+        } else {
+            $('#slider_open_button')
+                .animate({
+                    right: "-=250"
+                });
+            $('#right_slider')
+                .animate({
+                    right: "-=250"
+                });
+            slider_out = true;
+        }
+    });
