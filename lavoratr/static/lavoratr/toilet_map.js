296f4db82d79ea115a59d9df71ca10934d0176d6
harbieism <jow598@utulsa.edu>
2014-11-06 10:34:51 -0600

    Set up the js for an ajax call to get toilets.

--- lavoratr/static/lavoratr/toilet_map.js
+++ lavoratr/static/lavoratr/toilet_map.js
@@ -2,8 +2,24 @@ L.mapbox.accessToken = 'pk.eyJ1IjoiaGFyYmllaXNtIiwiYSI6IksyU1Rkc0UifQ.eXciAIxM0p
 
 var map = L.mapbox.map('map', 'harbieism.k45k1ejj').setView([40, -74.50], 9);
 
+map.on('moveend', function(e){
+	var bounds = map.getBounds();
+	var southWest = bounds.getSouthWest();
+	var northEast = bounds.getNorthEast();
+	var boundObject = {
+		southWestLat: southWest.lat,
+		southWestLng: southWest.lng,
+		northEastLat: northEast.lat,
+		northEastLng: northEast.lng,
+	}
+	$.get( 'get.geojson.js', boundObject, function(data) {
+		L.geoJson(data, {
+			onEachFeature: function(feature, layer) {
+				layer.bindPopup(feature.properties.location);
+			}
+		}).addTo(map);
+    });
 
-$.get( 'get.geojson.js', function(data) {
-	map.featureLayer.setGeoJSON(data);
 });
+
 
