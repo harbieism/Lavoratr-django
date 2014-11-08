2046c48a749bee8f316437b8349637babbb31af9
harbieism <jow598@utulsa.edu>
2014-11-08 14:42:25 -0600

    Revert "Added user geolocation and a draggable marker."
    This reverts commit 174bf00d94b65d6b12da568b17bd7052f6f247b9.

--- lavoratr/static/lavoratr/toilet_map.js
+++ lavoratr/static/lavoratr/toilet_map.js
@@ -1,35 +1,25 @@
-$( document ).ready(function() {
+L.mapbox.accessToken = 'pk.eyJ1IjoiaGFyYmllaXNtIiwiYSI6IksyU1Rkc0UifQ.eXciAIxM0pfdj5STBHNnbQ';
 
+var map = L.mapbox.map('map', 'harbieism.k45k1ejj').setView([40, -74.50], 9);
 
-
-	L.mapbox.accessToken = 'pk.eyJ1IjoiaGFyYmllaXNtIiwiYSI6IksyU1Rkc0UifQ.eXciAIxM0pfdj5STBHNnbQ';
-
-	var map = L.mapbox.map('map', 'harbieism.k45k1ejj');
-
-	navigator.geolocation.getCurrentPosition(function(position) {
-	  userLat = position.coords.latitude;
-	  userLng = position.coords.longitude;
-	  map.setView([userLat, userLng], 9)
-
-	  	var userMarker = L.marker(new L.LatLng(userLat, userLng), {
-		    icon: L.mapbox.marker.icon({
-		        'marker-color': 'ff8888'
-		    }),
-		    draggable: true
-		});
-
-		userMarker.addTo(map);
-	});
-
-
-	$.get( 'get.geojson.js', function(data) {
+map.on('moveend', function(e){
+	var bounds = map.getBounds();
+	var southWest = bounds.getSouthWest();
+	var northEast = bounds.getNorthEast();
+	var boundObject = {
+		southWestLat: southWest.lat,
+		southWestLng: southWest.lng,
+		northEastLat: northEast.lat,
+		northEastLng: northEast.lng,
+	}
+	$.get( 'get.geojson.js', boundObject, function(data) {
 		L.geoJson(data, {
 			onEachFeature: function(feature, layer) {
 				layer.bindPopup(feature.properties.location);
 			}
-	    }).addTo(map);
-
-	});
-
+		}).addTo(map);
+    });
 
 });
+
+
