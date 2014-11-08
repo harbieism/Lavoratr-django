9531a3a74a49dd6d9061ab01fb9bf08a0a78df50
harbieism <jow598@utulsa.edu>
2014-11-08 16:23:11 -0600

    Added a refresh button.

--- lavoratr/static/lavoratr/toilet_map.js
+++ lavoratr/static/lavoratr/toilet_map.js
@@ -1,6 +1,11 @@
 $( document ).ready(function() {
 
+    var geojsonLayer;
+    
 
+	$("#refresh").click( function () {
+	    getData();
+	})
 
 	L.mapbox.accessToken = 'pk.eyJ1IjoiaGFyYmllaXNtIiwiYSI6IksyU1Rkc0UifQ.eXciAIxM0pfdj5STBHNnbQ';
 
@@ -22,14 +27,18 @@ $( document ).ready(function() {
 	});
 
 
-	$.get( 'get.geojson.js', function(data) {
-		L.geoJson(data, {
-			onEachFeature: function(feature, layer) {
-				layer.bindPopup(feature.properties.location);
-			}
-	    }).addTo(map);
-
-	});
+	var getData = (function() {
+		$.get( 'get.geojson.js', function(data) {
+			geojsonLayer = L.geoJson(data, {
+				onEachFeature: function(feature, layer) {
+					layer.bindPopup(feature.properties.location);
+				}
+		    })
+		    geojsonLayer.addTo(map);
+		});
+    });
+    
+	getData();
 
 
 });
